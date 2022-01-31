45ab61e5deea5c5cd14e307c7f38f356a885a930
gregneat <gneat@gusd.net>
2022-01-31 13:56:02 -0800

    added p5 neatoBike

---
+++ AP/JavaSem2/097 Contrete classes - Bicycle w person NeatoBicycle/notes/p5NeatoBicycle/Vehicle.java
@@ -0,0 +1,19 @@
+import pkg.*;
+import java.util.ArrayList;
+
+
+
+public abstract class Vehicle{
+
+	public abstract void setStep(double yeet);
+	public abstract void drive();
+	public abstract void translate(double x, double y);
+	public abstract void draw();
+	public abstract int getX();
+	public abstract int getY();
+	public abstract int getWidth();
+	public abstract int getHeight();
+
+
+
+}
---
+++ AP/JavaSem2/097 Contrete classes - Bicycle w person NeatoBicycle/notes/p7 NeatoBicycle/Vehicle.java
@@ -0,0 +1,12 @@
+// import pkg.*;
+package pkg;
+public abstract class Vehicle{
+	public abstract void draw();
+	public abstract void fill();
+	public abstract void setStep(int x);
+	public abstract void translate(double x, double y);
+	public abstract double getX();
+	public abstract double getY();
+	public abstract void driveRandom();
+	public abstract void setColor(Color c);
+}
