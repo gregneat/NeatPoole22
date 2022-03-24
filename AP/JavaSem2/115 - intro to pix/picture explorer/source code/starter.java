c17b34bf9c49f33198e0ec35f197369442a1b063
gregneat <gneat@gusd.net>
2022-03-24 11:00:13 -0700

    added 2D array stuff

---
+++ AP/JavaSem2/115 - intro to pix/picture explorer/source code/starter.java
@@ -0,0 +1,22 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.image.*;
+import javax.swing.border.*;
+
+public class starter
+{
+  /**
+   * Test Main.  It will explore the beach 
+   */
+  public static void main( String args[])
+  {
+
+// put the name of you pix in the quotes below
+
+    Picture pix = new Picture("beach.jpg");
+    pix.explore();
+  }
+  
+}
---
+++ AP/JavaSem2/121- edge detection/base code/starter.java
@@ -0,0 +1,23 @@
+import pkg.*;
+
+public class starter implements InputControl {
+
+	
+
+	public static void main(String args[])
+        {
+			// following line is necessary for onMouseClick, don't change
+			MouseController mC = new MouseController(Canvas.getInstance(),new starter());
+			// put your code here:
+			
+		
+			
+		}
+
+
+		public void onMouseClick(double x, double y){
+			// and/or here
+			
+	
+		}
+}
